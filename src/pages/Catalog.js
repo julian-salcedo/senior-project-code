457f8591dad77ead532f30d738346789d3b91685
Julian Salcedo <jusalka2@gmail.com>
2022-02-11 15:43:19 -0500

    search bar

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Card from '../components/Card';
 
 function Catalog() {
   return (
     <div>
       <h1>Our Catalog</h1>
+      <form>
+        <input type='text' name='search-bar' id='search-bar' placeholder='Search...' />
+      </form>
+      <p>Book 1</p>
+      <p>Book 2</p>
+      <p>Book 3</p>
     </div>
   ) 
 }
