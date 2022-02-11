eea9833f461613e2a834084dfa2ea6a943c1560e
Julian Salcedo <jusalka2@gmail.com>
2022-02-11 14:16:32 -0500

    projects to catalog

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './components/Card'
 import Welcome from './pages/Welcome'
 import SignIn from './pages/SignIn'
-import Projects from './pages/Projects'
+import Catalog from './pages/Catalog'
 import Contact from './pages/Contact'
 import {Route} from 'react-router-dom'
 import NavBar from './components/NavBar'
@@ -22,7 +22,7 @@ function App() {
 
       <Route exact path='/' component={Welcome} />
       <Route exact path='/sign-in' component={SignIn} />
-      <Route exact path='/projects' component={Projects} />
+      <Route exact path='/catalog' component={Catalog} />
       <Route exact path='/contact' component={Contact} />
 
     </div>
