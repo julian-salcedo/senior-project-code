88d3f927f1ef4444fbae3fea97121f826ba6d699
Julian Salcedo <jusalka2@gmail.com>
2022-02-13 00:55:47 -0500

    added assets folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Card from './components/Card'
 import Welcome from './pages/Welcome'
 import SignIn from './pages/SignIn'
 import Catalog from './pages/Catalog'
@@ -15,12 +14,6 @@ function App() {
 
       <NavBar />
     
-      {/* <Card 
-        title='Card Title'
-        imageUrl='https://images.unsplash.com/photo-1541963463532-d68292c34b19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8Mnx8fGVufDB8fHx8&w=1000&q=80'
-        body='Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro ex voluptas eum error quia magni quisquam minima esse, culpa labore dolore eaque adipisci doloribus placeat rem quod hic iusto vero!' 
-      /> */}
-
       <Route exact path='/' component={Welcome} />
       <Route exact path='/sign-in' component={SignIn} />
       <Route exact path='/catalog' component={Catalog} />
