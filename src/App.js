65b884fc041d6c2109c02c292a9483ddeec783ec
Julian Salcedo <jusalka2@gmail.com>
2022-02-13 01:32:57 -0500

    initial design

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import SignIn from './pages/SignIn'
 import Catalog from './pages/Catalog'
 import MyBooks from './pages/MyBooks'
 import Admin from './pages/Admin'
-import {Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <div className='App'>
-
-      <NavBar />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={Welcome} />
+          <Route exact path='/sign-in' component={SignIn} />
+          <Route exact path='/catalog' component={Catalog} />
+          <Route exact path='/my-books' component={MyBooks} />
+          <Route exact path='/admin' component={Admin} />
+        </Switch>
+      </Router>
     
-      <Route exact path='/' component={Welcome} />
-      <Route exact path='/sign-in' component={SignIn} />
-      <Route exact path='/catalog' component={Catalog} />
-      <Route exact path='/my-books' component={MyBooks} />
-      <Route exact path='/admin' component={Admin} />
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import Logo from '../assets/logo-mcfatter.png';
+import '../styles/NavBar.css';
 
 function NavBar() {
   return (
-    <ul>
-        <li><Link to='/'>Welcome</Link></li>
-        <li><Link to='/sign-in'>Sign In</Link></li>
-        <li><Link to='/catalog'>Catalog</Link></li>
-        <li><Link to='/my-books'>My Books</Link></li>
-        <li><Link to='/admin'>Admin</Link></li>
-    </ul>
+    <div className='navbar'>
+      <div className='leftSide'>
+        <img src={Logo} />
+      </div>
+      <div className='rightSide'>
+        <Link to='/'>Welcome</Link>
+        <Link to='/sign-in'>Sign In</Link>
+        <Link to='/catalog'>Catalog</Link>
+        <Link to='/my-books'>My Books</Link>
+        <Link to='/admin'>Admin</Link>
+
+      </div>
+    </div>
+    
+
   );
 }
 
