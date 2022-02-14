7b35b446651a09b321ffb87a87eb7ec62a965a2a
Julian Salcedo <jusalka2@gmail.com>
2022-02-13 23:46:07 -0500

    functional hamburger

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../assets/logo-mcfatter.png';
+import ReorderIcon from '@material-ui/icons/Reorder';
 import '../styles/NavBar.css';
 
 function NavBar() {
+
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
   return (
     <div className='navbar'>
-      <div className='leftSide'>
+      <div className='leftSide' id={openLinks ? 'open' : 'closed'}>
         <img src={Logo} />
+        <div className='hiddenLinks'>
+          <Link to='/'>Welcome</Link>
+          <Link to='/sign-in'>Sign In</Link>
+          <Link to='/catalog'>Catalog</Link>
+          <Link to='/my-books'>My Books</Link>
+          <Link to='/admin'>Admin</Link>
+        </div>
       </div>
       <div className='rightSide'>
         <Link to='/'>Welcome</Link>
@@ -15,7 +29,9 @@ function NavBar() {
         <Link to='/catalog'>Catalog</Link>
         <Link to='/my-books'>My Books</Link>
         <Link to='/admin'>Admin</Link>
-
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
       </div>
     </div>
     
