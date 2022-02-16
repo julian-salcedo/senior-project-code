5ccdfff75982ad95857d93d1e1b630630a0e72f8
Julian Salcedo <jusalka2@gmail.com>
2022-02-16 14:23:33 -0500

    moved random-book-cover to assets folder

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Card from '../components/Card';
-import DefaultCover from '../random-book-cover.jpg';
+import DefaultCover from '../assets/random-book-cover.jpg';
 
 function Catalog() {
   return (
