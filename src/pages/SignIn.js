c899180132920650423e982c9cf3e3864cc4e685
ExcuseMeee <linoscar01@gmail.com>
2022-02-23 15:32:38 -0500

    auth

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,14 +1,30 @@
-import React from 'react';
-
-//test blh blah
+import {useState} from 'react';
+import { auth } from '../firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth'
 
 function About() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log('form submitted');
+    signInWithEmailAndPassword(auth, email, password)
+      .then((cred)=>{
+        console.log('user signed in', cred.user.email);
+      })
+      .catch((err)=>{
+        console.log(err.message);
+      })
+    
+  }
   return (
     <div>
       <h1>Sign In</h1>
-      <form action='/action_page.php'>
-        <input type='text' name='id'/><br/>
-        <input type='text' name='password'/><br/>
+      <form onSubmit={handleSubmit}>
+        <input required type='text' name='id' value={email} onChange={(e)=> setEmail(e.target.value)}/><br/>
+        <input required type='text' name='password' value={password} onChange={(e)=> setPassword(e.target.value)} /><br/>
         <input type='submit' value='Sign In' />
       </form>
     </div>
