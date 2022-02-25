1920a694185ca633be4a32eefaab3fd52682d1c1
Oscar Lin <linoscar01@gmail.com>
2022-02-24 23:30:26 -0500

    begin adding functionality to sign up

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,32 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/SignIn.css';
+import { auth } from '../firebaseConfig';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 function SignUp() {
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log('form submitted')
+
+    
+  }
   return (
     <div className='sign-in-page'>
       <h1>Sign Up</h1>
-      <form action='/action_page.php'>
+      <form onSubmit={handleSubmit}>
         <table>
+          <tbody>
             <tr>
                 <td>
                     <p>First Name</p>
-                    <input type='text' name='firstName' required/>
+                    <input required type='text' name='firstName' value={firstName} onChange={(e)=> setFirstName(e.target.value)}/>
                 </td>
                 <td>
                     <p>Last Name</p>
-                    <input type='text' name='lastName' required/>
+                    <input required type='text' name='lastName' value={lastName} onChange={(e)=> setLastName(e.target.value)}/>
                 </td>
             </tr>
             <tr>
                 <td>
                     <p>Email Address</p>
-                    <input type='email' name='email' required/>
+                    <input required type='email' name='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
                 </td>
                 <td>
                     <p>Password</p>
-                    <input type='text' name='password' required/><br/>
+                    <input required type='text' name='password' value={password} onChange={(e)=> setPassword(e.target.value)}/><br/>
                 </td>
             </tr>
+
+          </tbody>
         </table>
         <input type='submit' value='Sign Up' />
       </form>
