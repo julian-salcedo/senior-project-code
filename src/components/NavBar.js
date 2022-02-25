22f8a09892034ae8d05f1ba24ffbdc25dcbd2b1d
ExcuseMeee <linoscar01@gmail.com>
2022-02-25 15:25:54 -0500

    firebase funct to signup and signin

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import Logo from '../assets/logo-mcfatter.png';
 import ReorderIcon from '@material-ui/icons/Reorder';
 import '../styles/NavBar.css';
+import { signOut } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
 
 function NavBar() {
 
@@ -11,8 +13,20 @@ function NavBar() {
   const toggleNavbar = () => {
     setOpenLinks(!openLinks);
   };
+
+  function logOut(){
+    signOut(auth)
+      .then(()=>{
+        console.log('sign out clicked', auth.currentUser)
+      })
+      .catch((err)=>{
+        console.log(err.message)
+      })
+  }
   return (
     <div className='navbar'>
+        {/* testing */}
+        <button onClick={logOut}> sign out</button>
       <div className='leftSide' id={openLinks ? 'open' : 'closed'}>
         <img src={Logo} />
         <div className='hiddenLinks'>
