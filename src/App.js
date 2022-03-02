793f3128d8c3d84db9610daa3d1cc2c1d6017a9d
ExcuseMeee <linoscar01@gmail.com>
2022-03-02 15:50:44 -0500

    place hold functionality

--- src/App.js
+++ src/App.js
@@ -11,32 +11,41 @@ import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import { auth, db } from './firebaseConfig';
 import { onAuthStateChanged } from 'firebase/auth';
-import { getDoc, doc } from 'firebase/firestore';
+import { getDoc, doc, onSnapshot } from 'firebase/firestore';
 
 function App() {
 
   const [user, setUser] = useState(null);
+  const [uid, setUid] = useState(null);
+
   useEffect(()=>{
     onAuthStateChanged(auth, (userAuth)=>{
-      console.log('auth state change ran from app', userAuth)
+      //console.log('auth state change ran from app', userAuth)
       if(userAuth){
         const id = userAuth.uid;
         const docRef = doc(db, 'users', id)
+        setUid(id)
   
         getDoc(docRef)
           .then((doc)=> {
             setUser(doc.data())
-            console.log('getdoc ran', user)
+            //console.log('getdoc ran', user)
           })
           .catch((err)=> {
             console.log(err.message)
           })
-      }else{
+        onSnapshot(docRef, (doc)=> {
+          setUser(doc.data())
+          //console.log('onsnapshot in appjs ran: ', doc.data())
+
+        });
+        }else{
         setUser(null);
+        setUid(null);
       }
 
     })
-    console.log('app use effect ran: ', user)
+    //console.log('app use effect ran: ', user)
   }, [])
 
 
@@ -49,7 +58,7 @@ function App() {
           <Route exact path='/sign-in'><SignIn /></Route>
           <Route exact path='/sign-up' component={SignUp} />
           <Route exact path='/catalog' ><Catalog user={user}/></Route>
-          <Route exact path='/catalog/:id' component={BookInfo} />
+          <Route exact path='/catalog/:id' ><BookInfo user={user} uid={uid}/> </Route>
           <Route exact path='/my-books' component={MyBooks} />
           <Route exact path='/admin' component={Admin} />
         </Switch>
--- src/pages/BookInfo.js
+++ src/pages/BookInfo.js
@@ -3,11 +3,12 @@ import DefaultCover from '../assets/random-book-cover.jpg';
 import '../styles/BookInfo.css';
 import {useParams} from 'react-router-dom';
 import { db } from '../firebaseConfig';
-import { doc, onSnapshot, getDoc } from 'firebase/firestore';
+import { doc, getDoc, updateDoc } from 'firebase/firestore';
 import { useState, useEffect } from 'react';
 
-function BookInfo() {
+function BookInfo({user, uid}) {
 
+  //book id
   let { id } = useParams();
   const docRef = doc(db, 'books', id)
 
@@ -22,6 +23,26 @@ function BookInfo() {
         setIsPending(false);
       })
   }, []);
+
+  function placeHold(){
+    console.log('checkout clicked...');
+    if(user && uid != 'y4pfi7AYC7XwzsmgSKRLmF792VS2'){
+      if(user.books.some((obj) => { return (obj.bookId == id)})){
+        console.log('you already have this book: ', id)
+      }else{
+        console.log('checkout successful...')
+        const docRef = doc(db, 'users', uid)
+        updateDoc(docRef, {
+          books: [...user.books, {bookId: id, isCheckedOut: false}]
+        })
+          .then(() => console.log('update doc ran'))
+          .catch((err) => console.log(err.message))
+      }
+    }else{
+      console.log('you must be signed into student acc to checkout')
+    }
+    
+  }
   
   return (
     <div>
@@ -29,19 +50,16 @@ function BookInfo() {
 
       {bookInfo && 
       <div>
-        {/* <h2>{bookInfo.title}</h2>
-        <img src={DefaultCover} />
-        <p className='thing'>Hello</p> */}
         <div className='info-container'>
           <img className='cover' src={DefaultCover} />
           <h2 className='title'>{bookInfo.title}</h2>
           <p className='author'>
-            by <strong>Anita Nipane</strong>
+            by <strong>{bookInfo.author}</strong>
           </p>
           <p className='description'>
-            Most probably, while browsing a display of books on online bookstores, you have noticed that some of the books stand-out while others don’t. The design of these book covers somehow grab your attention and spur interest.
+            {bookInfo.desc}
           </p>
-          <a className='btn-checkout'>Checkout</a>
+          <a onClick={placeHold} className='btn-checkout'>Place Hold</a>
         </div>
       </div>
       }
