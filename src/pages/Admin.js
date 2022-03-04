a497d69e406979613146da067ccbc8bc0873fa28
Andre <bryceo4u123@gmail.com>
2022-03-04 15:01:26 -0500

    redesigned admin page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,34 +1,55 @@
 import React from 'react';
+import '../styles/Admin.css';
 
 function Admin() {
+  function selectForm(formName){
+    const forms = document.querySelectorAll('form');
+    forms.forEach(form => {
+      if(form.id === formName) {form.hidden = false}
+      else {form.hidden = true}
+    });
+  }
+
   return (
-    <div>
+    <div id='admin-page'>
       <h1>Admin</h1>
       <div>
-        <h3>Checkout Book</h3>
-        <form>
-            <div>User Id <input type="text" /></div>
-            <div>Book Id <input type="text" /></div> 
-            <div>Days Checked Out <input type="number" /></div> 
-            <button type="submit">Submit</button>
+        <h3 className='header' onClick={()=>selectForm("checkout-form")}>Checkout Book</h3>
+        <form id='checkout-form' hidden='true'>
+          <div>User Email <input type="text" /> <button>Get Holds</button></div>
+          <div>Reserved Books
+            <select>
+
+            </select>
+            <br /> OR
+          </div>
+          <div>Book Id <input type="text" /></div> 
+          <div>Days Checked Out <input type="number" /></div> 
+          <button type="submit">Submit</button>
         </form>
       </div>
       <div>
-        <h3>Return Book</h3>
-        <form>
-            <div>User Id <input type="text" /></div>
-            <div>Book Id <input type="text" /></div> 
-            <button type="submit">Submit</button>
-        </form>
-      </div>
-      <div>
-        <h3>Update Catalog</h3>
-        <form>
+        <h3 className='header' onClick={()=>selectForm("catalog-form")}>Update Catalog</h3>
+        <form id='catalog-form' hidden='true'>
             <div>Book Id<input type="text" /></div>
             <div>Title <input type="text" /></div> 
             <div>Author <input type="text" /></div> 
             <div>In Stock <input type="number" /></div> 
             <div><button type="submit">Add Book</button> <button type="submit">Update Book</button> <button type="submit">Delete Book</button></div>
+        </form>
+      </div>
+      <div>
+        <h3 className='header' onClick={()=>selectForm("return-form")}>Return Book</h3>
+        <form id='return-form' hidden='true'>
+          <div>User Email <input type="text" /> <button>Get Books</button></div>
+          <div>Checked Out Books 
+            <select>
+              
+            </select> 
+            <br /> OR
+          </div>
+          <div>Book Id <input type="text" /> </div>
+          <button type="submit">Submit</button>
         </form>
       </div>
     </div>
