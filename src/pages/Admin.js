65b6031f70484bd7be1693b29124336e79b3ea0f
ExcuseMeee <linoscar01@gmail.com>
2022-03-04 16:04:07 -0500

    admin functionality

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,27 +1,109 @@
-import React from 'react';
+import { db } from '../firebaseConfig';
+import { useState } from 'react';
+import { getDocs, addDoc, collection, query, where } from 'firebase/firestore';
+
 
 function Admin() {
+
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const [email, setEmail] = useState('');
+  const [bookId, setBookId] = useState('');
+
+  const booksColRef = collection(db, 'books')
+  const usersColRef = collection(db, 'users')
+  
+  const emailQuery = query(usersColRef, where("email", "==", email))
+  
+  function addBook(e){
+    e.preventDefault();
+    console.log('add book clicked');
+    if(amount <= 0){
+      console.log('invalid amount')
+    }else{
+      addDoc(booksColRef, {
+        title: title,
+        author: author,
+        desc: description,
+        amount: amount
+      })
+      .then(()=> {
+        console.log('adddoc ran')
+        resetComps();
+      })
+
+    }
+
+  }
+
+  function returnBook(e){
+    e.preventDefault();
+    console.log('return book clicked')
+    
+    getDocs(emailQuery).then((snapshot)=>{
+      const user = snapshot.docs[0];
+      if(user){
+        const id = user.id;
+        const data = user.data();
+        const books = data.books;
+        console.log(id, data, books);
+
+        const newBooks = books.filter((bookObj)=> {
+          return ((bookObj.bookId != bookId) || (bookObj.bookId == bookId && bookObj.isCheckedOut == 'false'));
+        })
+
+        console.log(newBooks);
+        //todo: update books field with newbooks
+
+
+        e.target.reset();
+        resetComps();
+      }else{
+        console.log('user no exist')
+      }
+      
+    })
+
+    
+  }
+
+  function resetComps(){
+    setTitle('');
+    setAuthor('');
+    setDescription('');
+    setAmount(0);
+    setEmail('');
+    setBookId('');
+    console.log('comps reset')
+  }
+
   return (
     <div>
       <h1>Admin</h1>
+
       <div>
         <h3>Checkout Book</h3>
         <form>
-            <div>User Id <input type="text" /></div>
-            <div>Book Id <input type="text" /></div> 
+            <div>Email <input required type="email" onChange={(e)=> setEmail(e.target.value)}/></div>
+            <div>Book Id <input required type="text" onChange={(e)=> setBookId(e.target.value)}/></div> 
             <div>Days Checked Out <input type="number" /></div> 
-            <button type="submit">Submit</button>
+            <input type="submit" value="Checkout"/>
         </form>
       </div>
+
       <div>
         <h3>Return Book</h3>
-        <form>
-            <div>User Id <input type="text" /></div>
-            <div>Book Id <input type="text" /></div> 
-            <button type="submit">Submit</button>
+        <form onSubmit={returnBook}>
+            <div>Email <input required type="email" onChange={(e)=> setEmail(e.target.value)}/></div>
+            <div>Book Id <input required type="text" onChange={(e)=> setBookId(e.target.value)}/></div> 
+            <input type="submit" value="Return book"/>
         </form>
       </div>
-      <div>
+
+      {/* <div>
         <h3>Update Catalog</h3>
         <form>
             <div>Book Id<input type="text" /></div>
@@ -29,6 +111,17 @@ function Admin() {
             <div>Author <input type="text" /></div> 
             <div>In Stock <input type="number" /></div> 
             <div><button type="submit">Add Book</button> <button type="submit">Update Book</button> <button type="submit">Delete Book</button></div>
+        </form>
+      </div> */}
+
+      <div>
+        <h3>Add book</h3>
+        <form onSubmit={addBook}>
+          <div>Title<input required type="text" value={title} onChange={(e)=> setTitle(e.target.value)}/></div>
+          <div>Author <input required type="text" value={author} onChange={(e)=> setAuthor(e.target.value)}/></div> 
+          <div>Description <input required type="text" value={description} onChange={(e)=> setDescription(e.target.value)}/></div> 
+          <div>In Stock <input required type="number" value={amount} onChange={(e)=> setAmount(e.target.value)}/></div>
+          <input type="submit" value="Add book"/>
         </form>
       </div>
     </div>
