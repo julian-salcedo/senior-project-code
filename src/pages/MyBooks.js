b15157d03397ccbd8db8a90e8458b6d3b7641f59
Julian Salcedo <jusalka2@gmail.com>
2022-03-04 14:19:22 -0500

    add card to page

--- src/pages/MyBooks.js
+++ src/pages/MyBooks.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Card from '../components/Card';
+import DefaultCover from '../assets/random-book-cover.jpg';
 
 function MyBooks() {
   return (
     <div>
       <h1>My Books</h1>
       <p>Total Overdue Fees:</p>
+      <h3>Checked Out:</h3>
+      <div className='wrapper'>
+        <Card image={DefaultCover} title='Test Book' author='person' />   
+      </div>
+      <h3>On Hold:</h3>
     </div>
   ) 
 }
