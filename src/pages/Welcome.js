f20476054829ab19cd1b044ae776059fcf5912af
Andre <bryceo4u123@gmail.com>
2022-03-05 12:55:48 -0500

    added a 'view your books' button on welcome page for logged in users

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -11,6 +11,7 @@ function Home({user}) {
       <h1>{(!user && "Welcome to the McFatter eLibrary") || ("Welcome back, " + user.first)}</h1>
       <div><img src={Storm_Logo} alt='McFatter Storm Logo'/></div>
       <div><Link to='catalog'><button>View Our Catalog</button></Link></div>
+      {user && <div><Link to='my-books'><button>View Your Books</button></Link></div>}
       {!user && <div><Link to='sign-up'><button>Sign Up</button></Link> <Link to='sign-in'><button>Sign In</button></Link></div>}
       <div><img src={Social_Media} alt='McFatter Social Media Handles' id='social-media' /></div>
       <div><a href='https://www.browardschools.com/mcfattertechhigh' target='_blank'><button>Visit Our Official Website</button></a></div>
