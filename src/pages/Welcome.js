7a76abf8552dff7fd3a3d2c3fe83c7a3e38f9616
Julian Salcedo <jusalka2@gmail.com>
2022-03-09 18:53:28 -0500

    set up

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -7,14 +7,18 @@ import Social_Media from '../assets/social-media-handles.png';
 
 function Home({user}) {
   return (
-    <div id='welcome-page'>
-      <h1>{(!user && "Welcome to the McFatter eLibrary") || ("Welcome back, " + user.first)}</h1>
-      <div><img src={Storm_Logo} alt='McFatter Storm Logo'/></div>
+    <div className='welcome-page'>
+      <div className='title'>
+        <strong>
+          {(!user && "Welcome to the McFatter eLibrary") || ("Welcome back, " + user.first)}
+        </strong>
+      </div>
+      {/* <div><img src={Storm_Logo} alt='McFatter Storm Logo'/></div>
       <div><Link to='catalog'><button>View Our Catalog</button></Link></div>
       {user && <div><Link to='my-books'><button>View Your Books</button></Link></div>}
       {!user && <div><Link to='sign-up'><button>Sign Up</button></Link> <Link to='sign-in'><button>Sign In</button></Link></div>}
       <div><img src={Social_Media} alt='McFatter Social Media Handles' id='social-media' /></div>
-      <div><a href='https://www.browardschools.com/mcfattertechhigh' target='_blank'><button>Visit Our Official Website</button></a></div>
+      <div><a href='https://www.browardschools.com/mcfattertechhigh' target='_blank'><button>Visit Our Official Website</button></a></div> */}
     </div>
   ); 
 }
