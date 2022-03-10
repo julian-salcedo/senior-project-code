5268ea16b86298516f501964fdeb3c74ed1a59e8
Julian Salcedo <jusalka2@gmail.com>
2022-03-10 00:45:53 -0500

    welcome page

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -8,18 +8,33 @@ import Social_Media from '../assets/social-media-handles.png';
 function Home({user}) {
   return (
     <div className='welcome-page'>
-      <div className='title'>
-        <strong>
-          {(!user && "Welcome to the McFatter eLibrary") || ("Welcome back, " + user.first)}
-        </strong>
+      <div className='welcome-top'>
+        <div className='welcome-title'>
+          <strong>
+            McFatter eLibrary      
+          </strong>
+        </div>
       </div>
-      {/* <div><img src={Storm_Logo} alt='McFatter Storm Logo'/></div>
-      <div><Link to='catalog'><button>View Our Catalog</button></Link></div>
-      {user && <div><Link to='my-books'><button>View Your Books</button></Link></div>}
-      {!user && <div><Link to='sign-up'><button>Sign Up</button></Link> <Link to='sign-in'><button>Sign In</button></Link></div>}
-      <div><img src={Social_Media} alt='McFatter Social Media Handles' id='social-media' /></div>
-      <div><a href='https://www.browardschools.com/mcfattertechhigh' target='_blank'><button>Visit Our Official Website</button></a></div> */}
-    </div>
+      <div className='welcome-bottom'>
+        <div className='welcome-info'>
+          <Link to='catalog'>
+            <button>
+              {(!user && "Checkout our Catalog") || ("Welcome " + user.first)}
+            </button>
+          </Link>
+        </div>
+        {user && <div className='welcome-books'>
+          <Link to='my-books'>
+            <button>View Your Books</button>
+          </Link>
+        </div>}
+        {!user && <div className='welcome-sign-in'>
+          <Link to='sign-in'>
+            <button>Sign In</button>
+          </Link>
+        </div>}
+      </div>
+     </div>
   ); 
 }
 
